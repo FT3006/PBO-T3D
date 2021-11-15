6e5bf32741c5ac477aba8867435d0d6d30efa34e
alfaz86 <alfaz@naetalab.com>
2021-11-15 11:49:33 +0700

    08 UTS

---
+++ A2.2000073/08_UTS/Main.py
@@ -0,0 +1,43 @@
+#----- 1. Variabel di Python -----#
+laptop = 2
+
+
+#----- 2. Mengubah Nilai variable -----#
+# angka_saya telah kita beri nilai 1945
+
+angka_saya = 1945
+
+# setelah didefinisikan dan diberi nilai
+# kita bisa mengubah nilainya
+
+angka_saya = 1
+
+# Untuk melihat kalau kita telah mengubahnya,
+# kita akan mencetaknya dengan print
+
+print(angka_saya)
+
+
+#----- 3. Pentingnya Spasi di Python -----#
+Komputer = 1
+laptop = 2
+
+
+#----- 4. Variable Yang Valid -----#
+P3MBU4T = "Guido van Rossum"  # nama variabel yang 'alay', tapi apakah valid?
+
+# variabel panjang nih
+contoh_perusahaan_yang_menggunakan_bahasa_python = "Youtube, Path, dan lain-lain."
+
+
+#----- 5. Tentang Class -----#
+class Kendaraan(object):
+    bahan_bakar = "bensin"
+
+
+#----- 6. Membuat Objek -----#
+class Kendaraan(object):
+    bahan_bakar = "bensin"
+
+mobil = Kendaraan()
+print(mobil.bahan_bakar)
