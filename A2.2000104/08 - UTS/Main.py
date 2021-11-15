c89929fbdfdb77c46efe77b25913029a430f1009
SariRetnakandini12 <A2.2000104@mhs.stmik-sumedang.ac.id>
2021-11-15 11:45:19 +0700

    8 UTS

---
+++ A2.2000104/08 - UTS/Main.py
@@ -0,0 +1,21 @@
+Laptop = 2
+
+angka_saya = 1945
+angka_saya = 1
+print(angka_saya)
+
+Komputer = 1
+Laptop = 2
+
+nama_bahasa = "python"
+P3MBUAT = "Guido van Rossum"
+Tahun_dibuatnya = 1991
+contoh_perusahaan_yang_menggunakan_bahasa_python = "Youtube, Path, dan lain-lain"
+belajar_python_oop = True
+Em4ng_s3ru = "Benar"
+
+class Kendaraan(object):
+    bahan_bakar = "Bensin"
+        
+mobil = Kendaraan()
+print(mobil.bahan_bakar)
