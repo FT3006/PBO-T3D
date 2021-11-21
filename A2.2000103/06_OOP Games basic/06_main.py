ff615c60e78e1e28e9f8b6779a1f30e57b73d75d
Anonym2800 <svamz34@gmail.com>
2021-11-21 15:48:08 +0700

    Tugas pada awal perkuliahan

---
+++ A2.2000103/06_OOP Games basic/06_main.py
@@ -0,0 +1,30 @@
+class Hero:
+    def __init__(self, name, health, attack, armor):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.armor = armor
+
+    def serang(self, lawan):
+        print(self.name + ' menyerang ' + lawan.name)
+        lawan.diserang(self)
+
+    def diserang(self, lawan):
+        print(self.name + ' diserang ' + lawan.name)
+        attack_diterima = lawan.attack/self.armor
+        print('serangan terasa : ' + str(attack_diterima))
+        self.health -= attack_diterima
+        print('darah ' + self.name + ' tersisa ' + str(self.health))
+
+sniper = Hero('sniper', 100, 10, 5)
+rikimaru = Hero('rikimaru', 100, 20, 10)
+
+sniper.serang(rikimaru)
+print('\n')
+rikimaru.serang(sniper)
+print('\n')
+rikimaru.serang(sniper)
+print('\n')
+rikimaru.serang(sniper)
+print('\n')
+rikimaru.serang(sniper)
