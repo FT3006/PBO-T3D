016655a64b05c1a13e0a7f1bb9b0a054a4763fbc
Anonym2800 <svamz34@gmail.com>
2021-11-21 15:10:10 +0700

    UTS

---
+++ A2.2000103/08_UTS/UTS.py
@@ -0,0 +1,48 @@
+#-------NO1 Variabel dipython
+
+laptop = 2
+
+
+#-------NO2 Mengubah Nilai variable
+
+# angka_saya telah kita beri nilai 1945 
+
+angka_saya = 1945
+
+# setelah didefinisikan dan diberi nilai 
+# kita bisa mengubah nilainya
+
+angka_saya = 1
+
+# untuk melihat kalau kita telah mengubahnya
+# kita akan mencetaknya dengan print 
+
+print (angka_saya)
+
+#------NO3 Pentingnya spasi diPython
+
+komputer = 4
+laptop = 3
+
+#------NO4 Variable Yang valid
+P3MBU4T = "Guido van Rossum" # nama variabel yang 'alay', tapi apakah valid?
+
+# variabel panjang nih
+contoh_perusahaan_yang_menggunakan_bahasa_python = "Youtube, Path, dan lain-lain."
+
+#------NO5 Tentang class 
+class Kendaraan(object):
+    bahan_bakar = "bensin"
+
+#------NO5 Membuat Objek
+class Kendaraan(object):
+    bahan_bakar = "bensin"
+
+mobil = Kendaraan()
+print(mobil.bahan_bakar)
+
+
+
+
+
+
