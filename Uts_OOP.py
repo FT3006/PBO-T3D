88d096b8e2511d3cd378c03f238979c770b6d22b
MuhamadAriansyah65 <91662253+MuhamadAriansyah65@users.noreply.github.com>
2021-11-26 16:16:41 +0700

    UTS-object oriented programming (OOP)

---
+++ Uts_OOP.py
@@ -0,0 +1,43 @@
+print("Uts Objek orientasi pemrograman")
+laptop = 2
+print (laptop)
+
+# 2. Mengubah Nilai Variabel
+
+# angka_saya telah kita beri nilai 1945
+
+angka_saya = 1945
+
+# setelah didefinisikan dan diberi nilai
+# kita bisa mengubah nilainya
+
+angka_saya = 1
+
+# Untuk melihat kalau kita telah mengubahnya,
+# kita akan mencetaknya dengan print
+
+print (angka_saya)
+
+# 3. Pentingnya Spasi Di Python
+komputer = 1
+laptop = 2
+
+print(komputer)
+print(laptop)
+
+# 4. Variabel yang Valid
+P3MBU4T = "Guido van Rossum" # nama variabel yang 'alay', tapi apakah valid?
+
+# variabel panjang nih
+contoh_perusahaan_yang_menggunakan_bahasa_python = "Youtube, Path, dan lain-lain."
+
+# 5. tentang class
+class Kendaraan(object):
+    bahan_bakar = "bensin"
+
+# 6. membuat objek
+class Kendaraan(object):
+    bahan_bakar = "bensin"
+
+mobil = Kendaraan()
+print(mobil.bahan_bakar) 
